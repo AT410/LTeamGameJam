76f9506306fbe03f7ebc10859ff9cbada58ed4d6
KoukiMatsuzaki <zeal18300086@c-league.jp>
2020-02-14 16:50:41 +0900

    専用シーン作成、テスト用、爆弾処理作成中

---
+++ GameJamProject/Assets/Kouki_Faile/Script/Bomb_bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb_bullet : MonoBehaviour
+{
+    float elapsedtime;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion RandomRot = Random.rotation;
+        elapsedtime = Time.deltaTime;
+        var angle = transform.eulerAngles;
+        var anglez = angle.z * (Mathf.PI / 180.0f);
+        Vector3 dir = new Vector3(Mathf.Cos(anglez), Mathf.Sin(anglez), 0.0f);
+        transform.position += new Vector3(transform.position.x ,transform.position.y, transform.position.z);
+    }
+}
---
+++ GameJamProject/Assets/Kouki_Faile/Script/Bomb_bullet_Prefab.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb_bullet_Prefab : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed;
+    float elapsedtime;
+    void BulletPrefab()
+    {
+        elapsedtime = Time.deltaTime;
+        Quaternion RandomRot = Random.rotation;
+        GameObject bullets = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BulletPrefab();
+    }
+}
