07cbdde053fdb488a05f4db10560715aec18fe6e
KoukiMatsuzaki <zeal18300086@c-league.jp>
2020-02-15 14:25:34 +0900

    タグ追加,爆弾弾丸反射完成

--- GameJamProject/Assets/Kouki_Faile/Script/Bomb_bullet.cs
+++ GameJamProject/Assets/Kouki_Faile/Script/Bomb_bullet.cs
@@ -6,21 +6,45 @@ public class Bomb_bullet : MonoBehaviour
 {
     float elapsedtime;
     public float speed;
-
+    private Vector2 lastvelocity;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
+    public float destroytime;
     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void BulletReflection()
     {
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Quaternion RandomRot = Random.rotation;
+        Vector2 refrectVec = Vector2.Reflect(this.lastvelocity, collision.contacts[0].normal);
+        rb.velocity = refrectVec;
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        lastvelocity = rb.velocity;
         elapsedtime = Time.deltaTime;
         var angle = transform.eulerAngles;
         var anglez = angle.z * (Mathf.PI / 180.0f);
         Vector3 dir = new Vector3(Mathf.Cos(anglez), Mathf.Sin(anglez), 0.0f);
-        transform.position += dir * speed * elapsedtime;
+        rb.AddForce(dir * speed);
+        Destroy(gameObject, destroytime);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 }
--- GameJamProject/Assets/Kouki_Faile/Script/Bomb_bullet_Prefab.cs
+++ GameJamProject/Assets/Kouki_Faile/Script/Bomb_bullet_Prefab.cs
@@ -7,22 +7,42 @@ public class Bomb_bullet_Prefab : MonoBehaviour
     public GameObject bullet;
     public float speed;
     float elapsedtime;
+    float max = 360.0f;
+    float min = 0.0f;
+    bool Explosion;
+    public int bulletCount;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            Debug.Log("Collision");
+
+        if(collision.gameObject.tag == "Playerbullet" || collision.gameObject.tag == "bomb")
+        {
+            Explosion = true;
+        }
+    }
     void BulletPrefab()
     {
-        elapsedtime = Time.deltaTime;
-        Quaternion RandomRot = Random.rotation;
-        GameObject bullets = Instantiate(bullet, transform.position, RandomRot) as GameObject;
-
+        if (Explosion == true)
+        {
+            elapsedtime = Time.deltaTime;
+            float RandomZ = Random.Range(min, max);
+            GameObject bullets = Instantiate(bullet, transform.position, Quaternion.Euler(0.0f, 0.0f, RandomZ)) as GameObject;
+            bulletCount -= 1;
     }
+}
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         BulletPrefab();
+        if (bulletCount <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
