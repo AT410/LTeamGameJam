c33f1c7d5a2d9e818d387aea4b029b550aeb564f
KoukiMatsuzaki <zeal18300086@c-league.jp>
2020-02-15 16:49:20 +0900

    改修

--- GameJamProject/Assets/Kouki_Faile/Script/Bomb_bullet.cs
+++ GameJamProject/Assets/Kouki_Faile/Script/Bomb_bullet.cs
@@ -7,12 +7,14 @@ public class Bomb_bullet : MonoBehaviour
     float elapsedtime;
     public float speed;
     private Vector2 lastvelocity;
+    private Vector2 VecPos;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     public float destroytime;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        VecPos = GetComponent<Transform>().position;
     }
 
     void BulletReflection()
@@ -22,9 +24,9 @@ public class Bomb_bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 refrectVec = Vector2.Reflect(this.lastvelocity, collision.contacts[0].normal);
-        rb.velocity = refrectVec;
-
+        //Vector2 refrectVec = Vector2.Reflect(this.lastvelocity, collision.contacts[0].normal);
+        //rb.velocity = refrectVec;
+        
         if (collision.gameObject.tag == "Player")
         {
             Destroy(gameObject);
@@ -35,12 +37,13 @@ public class Bomb_bullet : MonoBehaviour
     {
         lastvelocity = rb.velocity;
         elapsedtime = Time.deltaTime;
-        var angle = transform.eulerAngles;
+        var angle = transform.localEulerAngles;
         var anglez = angle.z * (Mathf.PI / 180.0f);
         Vector3 dir = new Vector3(Mathf.Cos(anglez), Mathf.Sin(anglez), 0.0f);
+        //transform.localPosition += dir * speed * elapsedtime;
         rb.AddForce(dir * speed);
         Destroy(gameObject, destroytime);
-
+        
     }
     // Update is called once per frame
     void Update()
